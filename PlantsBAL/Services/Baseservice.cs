d8d1c3487abaec3099875252affec7263a8ee46e
shereen-shaaban <sherinshaban67@gmail.com>
2026-05-07 13:33:34 +0300

    feat:add services

---
+++ PlantsBAL/Services/Baseservice.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlantsBAL.Services
+{
+    public interface IBaseservice<T> 
+    {
+		List<T> Getall();
+		void ADD(T t);
+		void Delete(T t);
+		void Edit(T t);
+        T Getbyid(int id);
+    }
+}
---
+++ PlantsBAL/Services/Genericeservice.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DAL.Repository;
+
+namespace PlantsBAL.Services
+{
+    public  class Genericeservice<T>:IBaseservice<T> where T : class
+
+    {
+        private readonly IbaseRepo<T> repo;
+
+        public Genericeservice(IbaseRepo<T> _repo)
+        {
+            repo = _repo;
+            
+        }
+
+        void IBaseservice<T>.ADD(T t)
+        {
+            repo.ADD(t);
+        }
+
+        void IBaseservice<T>.Delete(T t)
+        {
+            repo.Delete(t);
+        }
+
+        void IBaseservice<T>.Edit(T t)
+        {
+            repo.update(t);
+            
+        }
+
+        List<T> IBaseservice<T>.Getall()
+        {
+            return repo.GetAll();
+        }
+
+        T IBaseservice<T>.Getbyid(int id)
+        {
+			var t = repo.Getbyid(id);
+			if (t != null)
+				return t;
+			else
+				return null;
+		}
+    }
+}
