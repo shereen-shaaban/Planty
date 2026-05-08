ec9e1e4c0cb37e819b4b36308b62a904b2d52d7e
shereen-shaaban <sherinshaban67@gmail.com>
2026-05-08 08:09:57 +0300

    feat:add employee DTO

---
+++ PlantsDTO/DTO/EmployeeDTO/AddemployeeDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PlantsDAL.Validations.Employee;
+
+namespace PlantsDTO.DTO.EmployeeDTO
+{
+    public class AddemployeeDTO
+    {
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string address1 { get; set; }
+		[Employeeaddressvalidation]
+		public string address2 { get; set; }
+		//[EmployeeBDValidation]
+		public DateTime Birthdate { get; set; }
+		public string role { get; set; }
+	}
+}
---
+++ PlantsDTO/DTO/EmployeeDTO/GetEmployeeDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlantsDTO.DTO.EmployeeDTO
+{
+    public class GetEmployeeDTO
+    {
+		public string Name { get; set; }
+		public string address1 { get; set; }
+		public string address2 { get; set; }
+		public DateTime Birthdate { get; set; }
+		public string role { get; set; }
+	}
+}
